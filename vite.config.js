407ce054191ad859df09918fa3ef9c06df4463eb
Ruslan Skrypnyk <ruslan.skrypnyk.kita@donntu.edu.ua>
2025-10-30 15:55:14 +0200

    feat: fix config file for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Важливо для Azure
+  build: {
+    outDir: 'dist', // Папка для збірки
+    emptyOutDir: true, // Очищати перед збіркою
+    sourcemap: false, // Вимкнути sourcemaps для production
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      // Проксі для API запитів під час розробки
+      '/api': {
+        target: 'https://localhost:7000', // Ваш ASP.NET Core backend
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
