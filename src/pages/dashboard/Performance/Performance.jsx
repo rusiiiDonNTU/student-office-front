80bdb1e47ba0f28713c375d94df80d63f24b4530
rusiiiDonNTU <ruslan.skrypnyk.kita@donntu.edu.ua>
2025-12-31 21:46:42 +0200

    feat: fix (not) locales

--- src/pages/dashboard/Performance/Performance.jsx
+++ src/pages/dashboard/Performance/Performance.jsx
@@ -3,9 +3,7 @@ import "./Performance.css"
 function PerformancePage() {
   return (
     <section className="performance">
-      <div className="profile-header">
-        <h1>{t("profile:header")}</h1>
-      </div>
+
     </section>
   );
 }
