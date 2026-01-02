545ddc82ab0cfacf540dd1a63bf174a81ae022ff
rusiiiDonNTU <ruslan.skrypnyk.kita@donntu.edu.ua>
2026-01-02 15:33:38 +0200

    feat: fix email activation logic

--- src/components/Login/NotActivatedErrorModal/NotActivatedErrorModal.jsx
+++ src/components/Login/NotActivatedErrorModal/NotActivatedErrorModal.jsx
@@ -6,14 +6,14 @@ import { sendActivationList } from "../../../util/http";
 import { useState } from "react";
 import ModalButtons from "../../UI/Modal/ModalButtons/ModalButtons";
 import Button from "../../UI/Button/Button";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 function NotActivatedErrorModal({email, onClose = () => {}}) {
     const [isOpen, setIsOpen, handleClose] = useModal();
-    const {data, isPending, isError, error, isFetched, refetch} = useQuery({
+    const {mutate, isPending, isSuccess, isError, error} = useMutation({
         enabled: false,
-        queryKey: ["activation"],
-        queryFn: sendActivationList
+        mutationKey: ["activation"],
+        mutationFn: () => sendActivationList(email)
     });
     const { t } = useTranslation(["signin", "errors"]);
 
@@ -25,12 +25,12 @@ function NotActivatedErrorModal({email, onClose = () => {}}) {
     let header = t("signin:errors.notActivated.header")
     let body = <p>{t("signin:errors.notActivated.body")}<span>{email}</span></p>
 
-    if (!isError && data === 200) {
+    if (isSuccess) {
         header = t("signin:text.activationRequest.header")
         body = <p>{t("signin:text.activationRequest.beforeEmail")}<span>{email}</span>{t("signin:text.activationRequest.afterEmail")}</p>
     }
-    else {
-        if (isError && error.code === 429) {
+    else if (isError) {
+        if (error.code === 429) {
             header = t("signin:errors.tooManyTries.header")
             body = <p>{t("signin:errors.tooManyTries.body")}</p>
         }
@@ -40,13 +40,13 @@ function NotActivatedErrorModal({email, onClose = () => {}}) {
         }
     }
 
-    return <Modal isOpen={isOpen} onClose={handleConfirmErrorClose} isLoading={isLoading}>
+    return <Modal isOpen={isOpen} onClose={handleConfirmErrorClose} isLoading={isPending}>
         <h1>{header}</h1>
         <img src={emailImg} className="modal-img"/>
         {body}
         <ModalButtons>
             <Button onClick={handleConfirmErrorClose} disabled={isPending}>ОК</Button>
-            {!isFetched && <Button isBlue={true} onClick={refetch} disabled={isPending}>{t("signin:buttons.resend")}</Button>}
+            {(!isSuccess && !isError) && <Button isBlue={true} onClick={mutate} disabled={isPending}>{t("signin:buttons.resend")}</Button>}
         </ModalButtons>
     </Modal>
 }
